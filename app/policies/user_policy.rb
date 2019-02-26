9d592dd525428db0c887b9c6e5a8240168ce1aff
Elif Mentes <e.mentes88@gmail.com>
2019-02-26 12:23:00 +0000

    pundit methods are added to users policy and controller

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,4 +1,16 @@
 class UserPolicy < ApplicationPolicy
+  def show?
+    true
+  end
+
+  def edit?
+    record == user
+  end
+
+  def update?
+    edit?
+  end
+
   class Scope < Scope
     def resolve
       scope.all
