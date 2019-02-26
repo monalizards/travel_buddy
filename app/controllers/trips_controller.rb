3ec45e304d51a64ed6a593879e75e4f22394b321
Beatriz Barroso <beatrizscnbarroso@gmail.com>
2019-02-26 10:57:48 +0000

    trips controller done

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,44 @@
+class TripsController < ApplicationController
+  before_action :find_trip, only: [:show, :edit, :update, :destroy]
+  def index
+    @trips = Trip.all
+  end
+
+  def show
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def edit
+  end
+
+  def create
+    Trip.create(trip_params)
+
+    redirect_to trips_path
+  end
+
+  def update
+    @trip = Trip.update(trip_params)
+
+    redirect_to trips_path
+  end
+
+  def destroy
+    @trip.destroy
+
+    redirect_to trips_path
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:title, :description, :destination, :start_date, :end_date, :budget, :max_participants)
+  end
+
+  def find_trip
+    @trip = Trip.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'trips/index'
+  get 'trips/show'
+  get 'trips/new'
+  get 'trips/edit'
+  get 'trips/create'
+  get 'trips/update'
+  get 'trips/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
